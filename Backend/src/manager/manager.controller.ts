90b930c95b84087dec5778497aaaf7a6963ac8d3
Rimonkumar <22-46957-1@student.aiub.edu>
2025-07-21 00:07:49 +0600

    Update manager

--- Backend/src/manager/manager.controller.ts
+++ Backend/src/manager/manager.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { ManagerService } from './manager.service';
 
 @Controller('manager')
-export class ManagerController {}
+export class ManagerController {
+  constructor(private readonly managerService: ManagerService) {}
+
+  @Get()
+  getManager(): string {
+    return this.managerService.getManager();
+  }
+
+  @Get('/:id')
+  getManagerById(@Param('id') id: string): string {
+    return this.managerService.getManagerById(id);
+  }
+}
--- Backend/src/manager/manager.service.ts
+++ Backend/src/manager/manager.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class ManagerService {}
+export class ManagerService {
+  getManager(): string {
+    return 'Manager service is running successfully.';
+  }
+
+  getManagerById(id: string): string {
+    return 'Get manager by id: ' + id;
+  }
+
+  createManager(): string {
+    return 'Create manager';
+  }
+}

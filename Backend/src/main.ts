cd1f35329ca6b9f9339fc516066594b50e7bfad8
Tafsirul Islam Shafin <107920202+0SHAFIN@users.noreply.github.com>
2025-09-10 19:33:17 +0600

    Add global validation pipe to main application bootstrap

--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -10,6 +10,7 @@ async function bootstrap() {
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
   });
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
